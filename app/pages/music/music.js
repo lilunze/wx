ad7a0878723ca67d225b5f3f55f1da99491afa6d
llz <llz@wolvestribe.com>
2017-01-18 19:01:10 +0800

    add music list

--- app/pages/music/music.js
+++ app/pages/music/music.js
@@ -1,3 +1,88 @@
+var app=getApp();
+var utils=require('../../utils/utils.js');
 Page({
-    
+    data: 
+    {
+        newList: {},
+        recList: {},
+        hotList: {}
+    },
+
+    onLoad:function(event)
+    {
+        var newListUrl="http://www.duomi.com/list-list-getlist?id=user_new&pi=1&pz=8&_=2474545";
+        var recListUrl="http://www.duomi.com/list-list-getlist?id=user_rec&pi=1&pz=8&_=2474556";
+        var hotListUrl="http://www.duomi.com/list-list-getlist?id=user_hot&pi=1&pz=8&_=2474550";
+       
+        this.getMusicListData(newListUrl,'newList');
+        this.getMusicListData(recListUrl,'recList');
+        this.getMusicListData(hotListUrl,'hotList');       
+    },
+
+    getMusicListData:function(url,key)
+    {
+        var me=this;
+        wx.request
+        ({
+          url: url,
+          data: {},
+          method: 'GET',//OPTIONS,GET,HEAD,POST,PUT,DELETE, TRACE, CONNECT
+          header: {"Content-Type":"json"}, // 设置请求的 header
+          success: function(res)
+          {
+            // success
+            console.log(res);
+            me.processDuomiData(res.data,key);
+          },
+          fail: function(err) 
+          {
+            // fail
+            console.log(err);
+          }
+        })
+    },
+
+    processDuomiData:function(data,key)
+    {
+        var title="";
+        if(key=="newList")
+        {
+            title="最新歌单";
+        }
+        else if(key=="recList")
+        {
+            title="推荐歌单";
+        }
+        else
+        {
+            title="最热歌单";
+        }
+        var music=[];
+        for(var i in data.childs)
+        {
+            if(i<6)
+            {
+                var child=data.childs[i];
+                var temp=
+                {
+                name:child.name,
+                desc:child.description,
+                id:child.id,
+                coverageUrl:child.coverurl,
+                creator:child.creator.nick_name,
+                creatorImage:child.creator.portrait
+                };
+                music.push(temp);
+            }
+        }
+
+        var readyData={};
+        readyData[key]=
+        {
+            categoryTitle:title,
+            music:music
+        };
+        this.setData(readyData);
+    }
+
 })
--- app/pages/posts/post.js
+++ app/pages/posts/post.js
@@ -1,1 +1,64 @@
-Page({})
+var app=getApp();
+var utils=require('../../utils/utils.js');
+Page({
+    data: 
+    {
+        news:{}
+    },
+
+    onLoad:function(event)
+    {
+        var newsUrl="http://v.juhe.cn/toutiao/index?type=top&key=7695c5b9527a2b64f50de7be299c2170";
+       
+        this.getnewsListData(newsUrl);     
+    },
+
+    getnewsListData:function(url)
+    {
+        var me=this;
+        wx.request
+        ({
+          url: url,
+          data: {},
+          method: 'GET',//OPTIONS,GET,HEAD,POST,PUT,DELETE, TRACE, CONNECT
+          header: {"Content-Type":"json"}, // 设置请求的 header
+          success: function(res)
+          {
+            // success
+            console.log(res);
+            me.processNewsData(res.data);
+          },
+          fail: function(err) 
+          {
+            // fail
+            console.log(err);
+          }
+        })
+    },
+
+    processNewsData:function(data)
+    {
+        var news=[];
+        for(var i in data.result.data)
+        {
+            var newsItem=data.result.data[i];
+            var temp=
+            {
+               author:newsItem.author_name,
+               date:newsItem.date,
+               title:newsItem.title,
+               coverageUrl:newsItem.thumbnail_pic_s,
+               url:newsItem.url
+            };
+            news.push(temp);
+        }
+
+        var readyData={};
+        readyData['news']=
+        {
+            news:news
+        };
+        this.setData(readyData);
+    }
+
+})
